de0eec05cf5a823919418c823b529613a74a4827
Abik Mathew George <abhikmgeorge100@gmail.com>
2022-06-25 12:20:52 +0530

    basic details-employment complete

--- lib/view/pages/basic_details_employment/basic_details_employement.dart
+++ lib/view/pages/basic_details_employment/basic_details_employement.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:loans_for_bai/constants/color_constants.dart';
 import 'package:loans_for_bai/constants/ui_constants.dart';
-import 'package:loans_for_bai/view/pages/basic_details_employment/basic_details_employement.dart';
+import 'package:loans_for_bai/view/pages/basic_details_dob/basic_details_dob.dart';
 import 'package:loans_for_bai/view/widgets/custom_button.dart';
 import 'package:sizer/sizer.dart';
 import '../../widgets/custom_progress_indicator.dart';
@@ -10,8 +9,8 @@ import '../../widgets/custom_text_field.dart';
 import '../../widgets/image_widget.dart';
 import '../../widgets/text_widgets.dart';
 
-class BasicDetailsDob extends StatelessWidget {
-  const BasicDetailsDob({Key? key}) : super(key: key);
+class BasicDetailsEmployment extends StatelessWidget {
+  const BasicDetailsEmployment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,42 +23,29 @@ class BasicDetailsDob extends StatelessWidget {
           children: [
             const ImgWidget(),
             const CustomProgressIndicator(
-              progress: 0.5,
+              progress: 0.75,
             ),
             kHeight5,
             const SubHeading(
-              text: 'Apna janam dinn batayein',
+              text: 'Aap kitne ghar me khaam karte hain',
+              fontSize: 19,
             ),
             kHeight4,
-            Row(
-              children: [
-                kWidth15,
-                CustomTextField(
-                  hintText: 'DOB',
-                  width: 50,
-                  inputType: TextInputType.datetime,
-                  onChanged: (val) {
-                    // var a = val;
-                  },
-                ),
-                kWidth1,
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.calendar_today,
-                      color: kBlackBase,
-                      size: 35,
-                    ))
-              ],
+            CustomTextField(
+              height: 70,
+              onChanged: (val) {
+                // var a = val;
+              },
             ),
-            kHeight8,
+            kHeight5,
             Center(
               child: CustomButton(
                 onTap: () {
-                  Get.to(const BasicDetailsEmployment());
+                  Get.to(const BasicDetailsDob());
                 },
                 text: 'Aage badhein',
-                width: 75.w,
+                width: 80.w,
+                r: 20,
               ),
             ),
           ],
