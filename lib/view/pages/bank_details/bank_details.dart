8ef738ea16d122326dc3c98be6bb5793a60c3031
Abik Mathew George <abhikmgeorge100@gmail.com>
2022-06-25 17:58:20 +0530

    woking on bank-details ui

--- lib/view/pages/bank_details/bank_details.dart
+++ lib/view/pages/bank_details/bank_details.dart
@@ -1,58 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:loans_for_bai/constants/color_constants.dart';
 import 'package:loans_for_bai/constants/ui_constants.dart';
+import 'package:loans_for_bai/view/widgets/custom_button.dart';
 import 'package:loans_for_bai/view/widgets/custom_text_field.dart';
 import 'package:loans_for_bai/view/widgets/image_widget.dart';
 import 'package:loans_for_bai/view/widgets/text_widgets.dart';
+import 'package:sizer/sizer.dart';
 
-class BankDetails extends StatelessWidget {
+class BankDetails extends StatefulWidget {
   const BankDetails({Key? key}) : super(key: key);
 
   @override
+  State<BankDetails> createState() => _BankDetailsState();
+}
+
+bool checked = false;
+
+class _BankDetailsState extends State<BankDetails> {
+  @override
   Widget build(BuildContext context) {
-    bool _checked = false;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ImgWidget(),
-            Heading(
-              text: '6,000 aapke bank account Jama kiye jayenge!',
-              fontSize: 27,
-            ),
-            kHeight3,
-            Heading(text: 'Bank Name', fontSize: 24),
-            kHeight1,
-            CustomTextField(
-              onChanged: (vale) {},
-              hintText: 'Bank Name',
-            ),
-            kHeight2,
-            Heading(text: 'Account Number', fontSize: 24),
-            kHeight1,
-            CustomTextField(
-              onChanged: (vale) {},
-              hintText: 'Account Number',
-            ),
-            kHeight2,
-            Heading(text: 'Branch Address', fontSize: 24),
-            kHeight1,
-            CustomTextField(
-              onChanged: (vale) {},
-              hintText: 'Branch Address',
-            ),
-            CheckboxListTile(value: _checked, onChanged: (value){
-              setState
-            },
-            title: Text('Issi account se loan amount wapas hoga!'),
-            
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ImgWidget(),
+              const Heading(
+                text: '6,000 aapke bank account Jama kiye jayenge!',
+                fontSize: 27,
+              ),
+              kHeight3,
+              const Heading(text: 'Bank Name', fontSize: 24),
+              kHeight1,
+              CustomTextField(
+                onChanged: (vale) {},
+                hintText: 'Bank Name',
+              ),
+              kHeight2,
+              const Heading(text: 'Account Number', fontSize: 24),
+              kHeight1,
+              CustomTextField(
+                onChanged: (vale) {},
+                hintText: 'Account Number',
+              ),
+              kHeight2,
+              const Heading(text: 'Branch Address', fontSize: 24),
+              kHeight1,
+              CustomTextField(
+                onChanged: (vale) {},
+                hintText: 'Branch Address',
+              ),
+              kHeight3,
+              CustomCheckboxTile(),
+              Center(
+                child: CustomButton(
+                  onTap: () {},
+                  text: 'Account me transfer karein',
+                  width: 80.w,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Row CustomCheckboxTile() {
+    return Row(
+      children: [
+        SizedBox(
+          height: 50,
+          width: 20,
+          child: Checkbox(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              activeColor: kPurple,
+              checkColor: kWhite,
+              value: checked,
+              onChanged: (value) {
+                checked = value!;
+                setState(() {});
+              }),
+        ),
+        kWidth2,
+        const SubHeading(
+          text: 'Issi account se loan amount wapas hoga!',
+          fontSize: 15,
+        ),
+      ],
     );
   }
 }
